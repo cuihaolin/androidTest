b0b51f8f722258e0872d8e9521f19e59cc8f31f4
yueyue <2210654595@qq.com>
2017-03-14 11:20:58 +0800

    增加了一个吐司

--- app/src/main/java/com/m520it/com/myapplication/MainActivity.java
+++ app/src/main/java/com/m520it/com/myapplication/MainActivity.java
@@ -1,7 +1,8 @@
 package com.m520it.com.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "haha", Toast.LENGTH_SHORT).show();
     }
 }
