7d78e91df7c9a1e9294e133b1318d81f48d68d3d
yueyue <2210654595@qq.com>
2017-03-14 12:09:33 +0800

    newbranch中修改

--- app/src/main/java/com/m520it/com/myapplication/MainActivity.java
+++ app/src/main/java/com/m520it/com/myapplication/MainActivity.java
@@ -1,2 +1,16 @@
 package com.m520it.com.myapplication;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "haha", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "我是一个新的版本", Toast.LENGTH_SHORT).show();
+    }
+}
