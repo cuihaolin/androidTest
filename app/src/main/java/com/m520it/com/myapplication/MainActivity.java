6ea7417098979647e91ce8c7818d0a08b7b42aa7
yueyue <2210654595@qq.com>
2017-03-14 11:41:56 +0800

    搞破坏

--- app/src/main/java/com/m520it/com/myapplication/MainActivity.java
+++ app/src/main/java/com/m520it/com/myapplication/MainActivity.java
@@ -1,13 +1,2 @@
 package com.m520it.com.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
